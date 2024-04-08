25d2c48484ccf6ba4f4c637e859d3d27b0c42777
Tomasz Serafiński <work.serafinski@gmail.com>
2024-04-08 22:49:08 +0200

    init

---
+++ MP04/MP04/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MP04;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        //OGRANICZENIA
+        
+        //Ograniczenie Atrybutu
+        //Reguła określająca dopuszczalne operacje lub wartości dla atrybutu klasy,
+        //mająca na celu zapewnienie poprawności i spójności danych w systemie.
+        
+        //Ograniczenie Unique
+        //Atrybut posiada unikalną wartość w ramach ekstensji - może istnieć tylko jeden obiekt należący do danej klasy,
+        //który ma atrybut z daną wartością
+        
+        //Ograniczenie Subset - może być nakładane na dwie asocjacje (lub agregacje)
+        //Obydwie asocjacje powinny być pomiędzy tymi samymi klasami oraz obydwa powiązania powinny być pomiędzy tymi samymi obiektami
+        
+        //Ograniczenie Ordered - może być nakładane na asocjacje lub na klase
+        //Asocjacja - powiązania są przechowywane (oraz otrzymywane i przetwarzane) w pewnej ustalonej kolejności.
+        //Klasa - obiekty w ekstensji są przechowywane (oraz otrzymywane i przetwarzane) w pewnej ustalonej kolejności
+        
+        //Ograniczenie Bag - umożliwia przechowywanie duplikatów elementów
+        //Asocjacja - może istnieć wiele powiązań pomiędzy tymi samymi obiektami (niedozwolone w klasycznej asocjacji)
+        
+        //Ograniczenie Xor - dotyczy co najmniej 2 asocjacji
+        //Zapewnia, że będzie istniało tylko jedno powiązanie w ramach asocjacji, które ogranicza.
+        
+        //Ograniczenie Własne
+        //Niestandardowy warunek/ reguła wprowadzona do modelu systemu, określająca specyficzne wymagania dotyczące jego zachowania lub struktury,
+        //nieobjęte przez standardowe elementy UML.
+    }
+}
